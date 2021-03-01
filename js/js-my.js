15072e2d3d177260aad514d409ae8aecf6f0c95e
Vladislav Selyukov <41589224+LordWiseRus@users.noreply.github.com>
2021-03-01 22:48:54 +0300

    Автоматическое обновление возраста

---
+++ js/js-my.js
@@ -0,0 +1,43 @@
+//Нужные скрипты "под себя"
+//Перед добавлением нового настроить стили!!!
+
+
+/*<!--  Определение моего возраста
+        ======================================================================== -->*/
+var now = new Date();
+var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+var dob = new Date(2000, 3, 3);
+var dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate());
+var age;
+var txt;
+
+age = today.getFullYear() - dob.getFullYear();
+
+if (today < dobnow)
+{
+  age = age-1;
+}
+	
+count = age % 100;
+if (count >= 5 && count <= 20)
+{
+	txt = 'лет';
+}
+else
+{
+	count = count % 10;
+	if (count == 1)
+	{
+		txt = 'год';
+	}
+	else if (count >= 2 && count <= 4)
+	{
+		txt = 'года';
+	}
+	else
+	{
+		txt = 'лет';
+	}
+}
+
+document.write (`${age}` + ' ' + `${txt}`);
